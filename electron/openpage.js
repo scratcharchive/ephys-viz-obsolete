8d6c2c8726b5b3e7f19dee09cf1aa038b4a061bf
Jeremy Magland <jeremy.magland@gmail.com>
2018-05-09 08:55:45 -0400

    improve electron launching and support local file loading

---
+++ electron/openpage.js
@@ -0,0 +1,55 @@
+"use strict";
+
+var init_electron=require('./init_electron.js').init_electron;
+
+var CLP=new CLParams(process.argv);
+var html_fname=CLP.unnamedParameters[0]||'';
+if (!html_fname) {
+  throw new Error("Missing argument for html_fname");
+}
+
+var params=CLP.namedParameters;
+
+if (html_fname=='view_timeseries.html') {
+  params.timeseries=params.timeseries||CLP.unnamedParameters[1]||'';
+}
+else if (html_fname=='view_templates.html') {
+  params.templates=params.templates||CLP.unnamedParameters[1]||'';
+}
+
+var url=`file://${__dirname}/../web/${html_fname}`;
+init_electron(url,params);
+
+function CLParams(argv) {
+  this.unnamedParameters=[];
+  this.namedParameters={};
+
+  var args=argv.slice(2);
+  for (var i=0; i<args.length; i++) {
+    var arg0=args[i];
+    if (arg0.indexOf('--')===0) {
+      arg0=arg0.slice(2);
+      var ind=arg0.indexOf('=');
+      if (ind>=0) {
+        this.namedParameters[arg0.slice(0,ind)]=arg0.slice(ind+1);
+      }
+      else {
+        this.namedParameters[arg0]='';
+        if (i+1<args.length) {
+          var str=args[i+1];
+          if (str.indexOf('-')!=0) {
+            this.namedParameters[arg0]=str;
+            i++;  
+          }
+        }
+      }
+    }
+    else if (arg0.indexOf('-')===0) {
+      arg0=arg0.slice(1);
+      this.namedParameters[arg0]='';
+    }
+    else {
+      this.unnamedParameters.push(arg0);
+    }
+  }
+};
---
+++ web/electron_resources.js
@@ -0,0 +1,41 @@
+if (using_electron()) {
+	// It is annoying that we need to do this, but apparently necessary
+	window.$ = window.jQuery = require('jquery');
+
+	window.electron_resources={
+		load_binary_file_part:_load_binary_file_part
+	};	
+}
+
+function using_electron() {
+	var userAgent = navigator.userAgent.toLowerCase();
+	if (userAgent.indexOf(' electron/') > -1) {
+	   return true;
+	}
+	return false;
+}
+
+function _load_binary_file_part(path,start,end,callback) {
+	require('fs').open(path, 'r', function(err, fd) {
+		if (err) {
+		    callback(err.message);
+		    return;
+		}
+		if ((start===undefined)&&(end===undefined)) {
+			start=0;
+			end=get_file_size(path);
+		}
+		var buffer = new Buffer(end-start);
+		require('fs').read(fd, buffer, 0, end-start, start, function(err, num) {
+			if (err) {
+				callback(err.message);
+				return;
+			}
+			callback(null,buffer.buffer);
+		});
+	});
+}
+
+function get_file_size(fname) {
+	return require('fs').statSync(fname).size;
+}
