0d5659aad8f7eb11b175446442bb3594bc049a15
Jeremy Magland <jeremy.magland@gmail.com>
2018-07-16 09:16:00 -0400

    add gui test

--- electron/openpage.js
+++ electron/openpage.js
@@ -1,50 +1,54 @@
 "use strict";
 
-var init_electron=require('./init_electron.js').init_electron;
+var init_electron = require('./init_electron.js').init_electron;
 
-var CLP=new CLParams(process.argv);
-var html_fname=CLP.unnamedParameters[0]||'';
+var CLP = new CLParams(process.argv);
+var html_fname = CLP.unnamedParameters[0] || '';
 if (!html_fname) {
   throw new Error("Missing argument for html_fname");
 }
 
-var params=CLP.namedParameters;
-for (var jj=1; jj<CLP.unnamedParameters.length; jj++) {
-  params['arg'+(jj)]=CLP.unnamedParameters[jj];
+var params = CLP.namedParameters;
+for (var jj = 1; jj < CLP.unnamedParameters.length; jj++) {
+  params['arg' + (jj)] = CLP.unnamedParameters[jj];
 }
 
-var url=`file://${__dirname}/../web/${html_fname}`;
-init_electron(url,params);
+var url = `file://${__dirname}/../web/${html_fname}`;
+init_electron(url, params);
+
+if ('timeout' in CLP.namedParameters) {
+  // this is used for testing the gui
+  setTimeout(function() {
+    process.exit(0);
+  }, Number(CLP.namedParameters['timeout']));
+}
 
 function CLParams(argv) {
-  this.unnamedParameters=[];
-  this.namedParameters={};
+  this.unnamedParameters = [];
+  this.namedParameters = {};
 
-  var args=argv.slice(2);
-  for (var i=0; i<args.length; i++) {
-    var arg0=args[i];
-    if (arg0.indexOf('--')===0) {
-      arg0=arg0.slice(2);
-      var ind=arg0.indexOf('=');
-      if (ind>=0) {
-        this.namedParameters[arg0.slice(0,ind)]=arg0.slice(ind+1);
-      }
-      else {
-        this.namedParameters[arg0]='';
-        if (i+1<args.length) {
-          var str=args[i+1];
-          if (str.indexOf('-')!=0) {
-            this.namedParameters[arg0]=str;
-            i++;  
+  var args = argv.slice(2);
+  for (var i = 0; i < args.length; i++) {
+    var arg0 = args[i];
+    if (arg0.indexOf('--') === 0) {
+      arg0 = arg0.slice(2);
+      var ind = arg0.indexOf('=');
+      if (ind >= 0) {
+        this.namedParameters[arg0.slice(0, ind)] = arg0.slice(ind + 1);
+      } else {
+        this.namedParameters[arg0] = '';
+        if (i + 1 < args.length) {
+          var str = args[i + 1];
+          if (str.indexOf('-') != 0) {
+            this.namedParameters[arg0] = str;
+            i++;
           }
         }
       }
-    }
-    else if (arg0.indexOf('-')===0) {
-      arg0=arg0.slice(1);
-      this.namedParameters[arg0]='';
-    }
-    else {
+    } else if (arg0.indexOf('-') === 0) {
+      arg0 = arg0.slice(1);
+      this.namedParameters[arg0] = '';
+    } else {
       this.unnamedParameters.push(arg0);
     }
   }
