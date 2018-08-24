85c0ce935290fc1e03770fa3261cf887fab0aaab
Jeremy Magland <jeremy.magland@gmail.com>
2018-08-23 23:32:21 -0400

    sync version between python and js

--- jp_ephys_viz/setup.py
+++ jp_ephys_viz/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="jp_ephys_viz",
-    version="0.6.11",
+    version="0.6.15",
     author="Jeremy Magland",
     author_email="jmagland@flatironinstitute.org",
     description="Use ephys-viz in jupyterlab and ipython notebook",
